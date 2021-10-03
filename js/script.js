6ec5894ce9394292301e9722020061c95fb62a98
adetounabims <titoabimbola@gmail.com>
2021-10-03 19:30:11 +0100

    Updated

--- js/script.js
+++ js/script.js
@@ -62,6 +62,44 @@ const makeGuess = function(formInput) {
     } else {
         guessedLetters.push(formInput);
         console.log(guessedLetters);
+        updatePage();
+        updateWordInProgress(guessedLetters);
     };
     
+};
+
+//function to update the page with letters guessed
+const updatePage = function () {
+    ulList.innerHTML = "";
+    for (const item of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = item;
+        ulList.append(li);
+    }
+};
+
+//function to update the word in progress
+const updateWordInProgress = function ( guessedLetters ) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    console.log(wordArray);
+    const showGuessedLetters = [];
+    for (const letter of wordArray) {
+    if (guessedLetters.includes( letter ) ) {
+        showGuessedLetters.push(letter.toUpperCase());
+    } else {
+        showGuessedLetters.push("●");
+    }
+    }
+    wordProgress.innerText = showGuessedLetters.join(""); 
+    wonGame();
+};
+
+//function to check if player won
+const wonGame = function () {
+    if ( word.toUpperCase() === wordProgress.innerText ) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">
+        You guessed the correct word! Congrats!</p>`;
+    }
 };
